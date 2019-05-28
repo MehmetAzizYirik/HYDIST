5049a8a38c68d4931534abe4bceec36d6a0407f4
MehmetAzizYirik <mehmetazizyirik@outlook.com>
2019-05-28 13:47:09 +0200

    Small change

--- src/main/java/hydrogenDistributor/HydrogenDistributor.java
+++ src/main/java/hydrogenDistributor/HydrogenDistributor.java
@@ -1,7 +1,7 @@
 /**
  * MIT License
  *
- * Copyright (c) 2019 Mehmet Aziz Yirik
+ * Copyright (c) 2018 Mehmet Aziz Yirik
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
